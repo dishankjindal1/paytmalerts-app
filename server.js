787bed152c14f8f9dfc9de70c567a9685e1b2b71
Dishank Jindal <iamprojectro@gmail.com>
2019-06-14 02:26:44 +0530

    setting

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://streamlabs.com");
   res.header("Access-Control-Allow-Methods", "GET,POST");
-  res.header("Access-Control-Allow-Headers", "Origin, x-requested-with, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Custom-Header");
   next();
 });
 
