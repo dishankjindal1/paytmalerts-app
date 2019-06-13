70133e10416c440ab7cc0e9714425be0433e4ff5
Dishank Jindal <iamprojectro@gmail.com>
2019-06-14 02:53:49 +0530

    adding CORS Support

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(function(req, res, next) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
-  res.header("Access-Control-Allow-Headers", "X-Custom-Header");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
   next();
 });
 
