8c2053f5a42fe904f5495903dda4d88b6f56e8f2
Dishank Jindal <iamprojectro@gmail.com>
2019-06-14 01:53:20 +0530

    changing again

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Expose-Headers", "WWW-Authenticate, Server-Authorization");
   next();
 });
 
