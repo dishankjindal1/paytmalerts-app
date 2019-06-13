dc2e62b7f2a77938298350c6804453cae70da1cd
Dishank Jindal <iamprojectro@gmail.com>
2019-06-14 01:41:48 +0530

    fixed content type error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
