eb624d20938062b1781f493f1988fef9e22e9d50
Dishank Jindal <iamprojectro@gmail.com>
2019-06-14 02:50:37 +0530

    hi

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://streamlabs.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST");
   res.header("Access-Control-Allow-Headers", "X-Custom-Header");
   next();
