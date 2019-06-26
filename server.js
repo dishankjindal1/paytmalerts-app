39e6204c1436079f357fbb222a71423539406458
Dishank Jindal <iamprojectro@gmail.com>
2019-06-26 23:54:19 +0530

    fixing google login

--- server.js
+++ server.js
@@ -13,17 +13,13 @@ app.set('view engine', 'jade');
 
 
 app.use(logger('dev'));
-/*
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-*/
 app.use(express.static(path.join(__dirname, 'app')));
 
-/*
 app.use('*', cors());
 app.use(cors());
-*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
